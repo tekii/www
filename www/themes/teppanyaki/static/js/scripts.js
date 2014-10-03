563ad35c89af16edff484b32c744a5330e9bc66a
Marcelo Glezer <mg@tekii.com.ar>
2014-10-03 17:39:01 -0300

    agregue bootstrap.js y jquery 1.11 removi compiled.js y pase la parte que necesitabamos a scripts.js

---
+++ www/themes/teppanyaki/static/js/scripts.js
@@ -0,0 +1,69 @@
+				var debounce = function (func, threshold, execAsap) {
+			 
+			    var timeout;
+			 
+			    return function debounced () {
+			        var obj = this, args = arguments;
+			        function delayed () {
+			            if (!execAsap)
+			                func.apply(obj, args);
+			            timeout = null; 
+			        };
+			 
+			        if (timeout)
+			            clearTimeout(timeout);
+			        else if (execAsap)
+			            func.apply(obj, args);
+			 
+			        timeout = setTimeout(delayed, threshold || 100); 
+			    };
+			 
+			}
+	    $('#hero-wrapper').videoBG({
+	    	mp4:'/videos/home.mp4',
+	    	ogv:'/videos/home.ogv',
+	    	webm:'/videos/home.webm',
+	    	poster:'/videos/home.jpg',
+	    	scale:true,
+        width: "100%",
+        height: "100%",        
+	    	zIndex:0
+	    });
+      f = $("header#site-header nav");
+      e = $(window),
+      i = function() {
+        return $("button.navbar-toggle").is(":visible") ? 40 : 80;
+      };
+      h = function() {
+      	console.log("f.length:"+f,length)
+        if (f.length) {
+          var a = e.scrollTop(),
+            b = i();
+            console.log("sticking")
+          a > b && !f.hasClass("sticky") ? f.addClass("sticky") : b > a && f.hasClass("sticky") && !f.data("is-showing") && f.removeClass("sticky")
+        }
+      };
+ 			/* k = function() {
+            f.length && f.on("show.bs.collapse", function() {
+              f.data("is-showing", !0), f.hasClass("sticky") || f.addClass("sticky");
+              var a = e.height() - f.height() - 10;
+              $("div.navbar-collapse").css("max-height", a + "px")
+            }).on("hide.bs.collapse", function() {
+              f.data("is-showing", !1);
+              var a = e.scrollTop(),
+                b = i();
+              b > a && f.hasClass("sticky") && f.removeClass("sticky")
+            })
+          }
+        p = function() {
+            $("a[data-scroll-to]").on("click", function(a) {
+              var b = $(this),
+                c = $(b.data("scroll-to")).show();
+              "undefined" != b.attr("data-scroll-to-disappear") && b.parent().remove(), $("html,body").animate({
+                scrollTop: c.offset().top - 106
+              }), a.preventDefault()
+            })
+          }           */
+			e.on("scroll", debounce(h, 100, false)); 
+			 /* k();  */
+       /* p();  */
